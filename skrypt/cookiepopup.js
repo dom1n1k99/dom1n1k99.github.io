17e3f5e07242ac36146a4228d818925ab681a262
dom1n1k99 <71665159+dom1n1k99@users.noreply.github.com>
2023-05-19 14:31:37 +0200

    Update cookiepopup.js

--- skrypt/cookiepopup.js
+++ skrypt/cookiepopup.js
@@ -54,8 +54,7 @@ function eraseCookie(name) {
 
 function cookiePopupBox() {
   if (!getCookie('cookiePopupDismiss')) {
-    document.body.innerHTML += '<div class="cookiePopupBoxContainer" id="cookiePopupBoxContainer"><div class="cookieTitle"><p>' + cookiePopupTitle + '</p></div><div class="cookieDesc"><p>' + cookiePopupDesc + ' ' + cookiePopupLink + '</p></div><div class="cookieButton"><a href=
-"#" onClick="cookiePopupDismiss();">' + cookiePopupButton + '</a></div></div>';
+    document.body.innerHTML += '<div class="cookiePopupBoxContainer" id="cookiePopupBoxContainer"><div class="cookieTitle"><p>' + cookiePopupTitle + '</p></div><div class="cookieDesc"><p>' + cookiePopupDesc + ' ' + cookiePopupLink + '</p></div><div class="cookieButton"><a onClick="cookiePopupDismiss();" href="#">' + cookiePopupButton + '</a></div></div>';
 	pureFadeIn("cookiePopupBoxContainer");
   }
 }
